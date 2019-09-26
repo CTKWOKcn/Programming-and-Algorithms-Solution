1443dc1acb21404af184d12c35641a0d2f9771d0
CTKwok <1104350529@qq.com>
2019-09-26 12:19:53 +0800

    2.0

---
+++ Programming and Algorithms Ⅱ/Hanio.cpp
@@ -0,0 +1,22 @@
+//#include<memory>
+//#include<string>
+//#include<cstring>
+//#include<iostream>
+//using namespace std;
+//void Hanio(int n, char src, char mid, char dest) {
+//	if (n == 1) {
+//		cout << src << "->" << dest << endl;
+//		return;
+//	}
+//	Hanio(n - 1, src, dest, mid);
+//	cout << src << "->" << dest << endl;
+//	Hanio(n - 1, mid, src, dest);
+//	return;
+//}
+//int main() {
+//	int n;
+//	cin >> n;
+//	Hanio(n, 'a', 'b', 'c');
+//
+//	return 0;
+//}
---
+++ Programming and Algorithms Ⅱ/熄灯问题(myself)2.0.cpp
@@ -0,0 +1,6 @@
+#include<memory>
+#include<string>
+#include<cstring>
+#include<iostream>
+using namespace std;
+int a[5];
