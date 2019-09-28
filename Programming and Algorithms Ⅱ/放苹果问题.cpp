d4fd1b72139da5a496abbb5851066acfd004ca1c
CTKwok <1104350529@qq.com>
2019-09-28 19:07:32 +0800

    2019年9月28日

---
+++ Programming and Algorithms Ⅱ/放苹果问题.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//using namespace std;
+//int i, k;
+//int A(int i, int k) {
+//	if (i == 0)
+//		return 1;
+//	if (k == 0)
+//		return 0;
+//	if (k > i)
+//		return A(i, i);
+//	return A(i, k - 1) + A(i - k, k);
+//}
+//int main() {
+//	cin >> i >> k;
+//	cout << A(i, k) << endl;
+//	return 0;
+//}
---
+++ Programming and Algorithms Ⅱ/跳台阶问题.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//using namespace std;
+//int n;
+//int stairs(int n) {
+//	if (n==1)
+//		return 1;
+//
+//	if (n == 2)
+//		return 2;
+//
+//	return stairs(n - 1) + stairs(n - 2);
+//}
+//int main() {
+//	cin >> n;
+//	cout << stairs(n) << endl;
+//	return 0;
+//}
